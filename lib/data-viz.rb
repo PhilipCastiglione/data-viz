86e09ca62f407e4cb2ea2a74c6850d27801713db
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-04 15:54:14 +1000

    main class employs fetcher

--- lib/data-viz.rb
+++ lib/data-viz.rb
@@ -1,16 +1,18 @@
-require 'rubygems'
-require 'bundler'
+require "rubygems"
+require "bundler"
 Bundler.require
 Dotenv.load
 
-require_relative './data-viz/fetch'
-require_relative './data-viz/present'
-require_relative './data-viz/transform'
+require_relative "./data-viz/fetcher"
+require_relative "./data-viz/transformer"
+require_relative "./data-viz/presenter"
 
 class DataViz
   def call
-    p 'wat'
-    p ENV['SECRET_THING']
+    fetcher = Fetcher.new
+    serialized_time_entries = fetcher.call
+
+
   end
 end
 
