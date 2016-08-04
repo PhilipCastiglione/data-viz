a5f1199c8f391e266ca72960e19267da59180377
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-04 15:56:13 +1000

    basic skeleton for transformer and preseenter

--- lib/data-viz.rb
+++ lib/data-viz.rb
@@ -8,9 +8,17 @@ require_relative "./data-viz/transformer"
 require_relative "./data-viz/presenter"
 
 class DataViz
+  attr_accessor :fetcher, :transformer, :presenter
+
+  def initialize
+    self.fetcher = Fetcher.new
+    self.transformer = Transformer.new
+    self.presenter = Presenter.new
+  end
+
   def call
-    fetcher = Fetcher.new
-    serialized_time_entries = fetcher.call
+    #serialized_time_entries = fetcher.call
+
 
 
   end
