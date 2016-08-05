ceab8fa6c09e1277d010c9512b646804bbc72ac3
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-05 15:52:49 +1000

    moved some code around i guess

--- lib/data-viz/presenter.rb
+++ lib/data-viz/presenter.rb
@@ -1,29 +1,48 @@
 class Presenter
-  attr_accessor :date, :tasks, :summary_tasks, :dates
+  attr_accessor :date, :tasks, :summary_tasks, :dates, :data, :summary_data
 
   def initialize(date)
     self.date = date
     self.tasks = []
     self.summary_tasks = []
     self.dates = []
+    self.data = {}
+    self.summary_data = {}
   end
 
   def generate_charts
-    tasks = []
-    summary_tasks = []
-    dates = []
+    extract_tasks_and_dates
+
+    prepare_data_structure
+
+    populate_data
+  
+    generate_images
+  end
+
+  private
+
+  def load_csv(&block)
+    CSV.foreach("./csv/#{date}.csv",{ headers: true }, &block)
+  end
+
+  def extract_tasks_and_dates
     load_csv do |row|
-      tasks << row["Task"]
-      summary_tasks << task_subset(row["Task"])
-      dates << row["Date"]
+      self.tasks << row["Task"]
+      self.summary_tasks << task_subset(row["Task"])
+      self.dates << row["Date"]
     end
-    tasks.uniq!
-    summary_tasks.uniq!
-    dates.uniq!
+    self.tasks.uniq!
+    self.summary_tasks.uniq!
+    self.dates.uniq!
+  end
 
-    data = tasks.reduce({}) { |h, t| h.merge(t => [0] * dates.size) }
-    summary_data = summary_tasks.reduce({}) { |h, t| h.merge(t => [0] * dates.size) }
+  def prepare_data_structure
+    self.data = tasks.reduce({}) { |h, t| h.merge(t => [0] * dates.size) }
+    self.summary_data = summary_tasks.reduce({}) { |h, t| h.merge(t => [0] * dates.size) }
+  end
 
+  def populate_data
     day_num = 0
     day = ''
     load_csv do |row|
@@ -35,7 +54,19 @@ class Presenter
       data[row["Task"]][day_num - 1] += row["Hours"].to_f
       summary_data[task_subset(row["Task"])][day_num - 1] += row["Hours"].to_f
     end
+  end
 
+  def task_subset(task)
+    case task
+    when /^Work/                      then "Work"
+    when /Dead/, /Travel/, /Chore/    then "Boring Life Things"
+    when /Leisure/, /Vino/, /Sociali/ then "Fun"
+    else
+      task
+    end
+  end
+
+  def generate_images
     chart = Gruff::StackedBar.new
     chart.title = "Harvest Timesheet Detailed Report"
     chart.labels = Hash[(0...dates.size).zip dates.map { |d| d.slice(5, 9) }]
@@ -47,28 +78,5 @@ class Presenter
     chart.labels = Hash[(0...dates.size).zip dates.map { |d| d.slice(5, 9) }]
     summary_data.each { |task, hours| chart.data(task, hours) }
     chart.write("./charts/#{date}_summary.png")
-  end
-
-  private
-
-  def load_csv(&block)
-    CSV.foreach("./csv/#{date}.csv",{ headers: true }, &block)
-  end
-
-  def task_subset(task)
-    case task
-    when /^Work/
-      then "Work"
-    when /Dead/,
-         /Travel/,
-         /Chore/
-      then "Boring Life Things"
-    when /Leisure/,
-         /Vino/,
-         /Sociali/
-      then "Fun"
-    else
-      task
-    end
   end
 end
